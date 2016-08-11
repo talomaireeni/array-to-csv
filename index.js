fd34a6b6d2b0e6a2b3ae7131b724d5e739a9acac
Brian Pow <brianpow@gmail.com>
2016-08-12 00:03:51 +0800

    code reformat

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict'
 if (!Object.values)
-    require('object.values/shim')();
+  require('object.values/shim')()
 
-module.exports = function(array, separator) {
-    separator = separator || ','
-    return (array.hasOwnProperty("length")) ? array.map(joinRow, separator).join('\n') : Object.values(array).map(joinRow, separator).join('\n')
+module.exports = function (array, separator) {
+  separator = separator || ','
+  return (array.hasOwnProperty('length')) ? array.map(joinRow, separator).join('\n') : Object.values(array).map(joinRow, separator).join('\n')
 }
 
 function joinRow(row) {
-    return row.map(escapeCell, this).join(this)
+  return row.map(escapeCell, this).join(this)
 }
 
 function escapeCell(cell) {
-    cell = cell || ''
-    if (typeof cell === 'function') cell = cell()
-    cell = cell.toString()
-    return (cell.indexOf(this) !== -1 || cell.indexOf('"') !== -1) ?
-        '"' + cell.replace(/\"/g, '""') + '"' :
-        cell
+  cell = cell || ''
+  if (typeof cell === 'function') cell = cell()
+  cell = cell.toString()
+  return (cell.indexOf(this) !== -1 || cell.indexOf('"') !== -1) ?
+    '"' + cell.replace(/\"/g, '""') + '"' :
+    cell
 }
